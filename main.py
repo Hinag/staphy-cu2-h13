09c2515fba5c9e45f3429b9769e0d41fa981bf4f
Felix Springer <felixspringer149@gmail.com>
2018-12-03 11:31:41 +0100

    added comments and critical temp

--- main.py
+++ main.py
@@ -14,30 +14,31 @@ import matplotlib.animation as animation
 import random
 
 class grid:
-    """Klasse für Spin-Gitter"""
+    """class for spin-lattice"""
 
     def __init__(self, xdim, ydim):
-        """erstellt Gitter mit zufälligen Einträgen"""
+        """creates lattice with random spins"""
         self.xdim = xdim
         self.ydim = ydim
         self.lattice = np.random.randint(low=0, high=2, size=(xdim, ydim))
-        self.lattice = 2 * self.lattice - 1
-        self.h = 0
-        self.tau = 1
-        self.speed = 1
+        self.lattice = 2 * self.lattice - 1 # lattice with random 1 and -1
+        self.h = 0 # magnetic field
+        self.tau = 1 # temperature
+        self.speed = 1 # speed of time progression
         return
 
     def print_settings(self):
-        """gibt Informationen über das Gitter and stdout"""
+        """gives information about lattice to stdout"""
         print("Gittergröße: ",self.xdim, "x",self.ydim)
         print(self.lattice)
+        return
 
     def update(self):
-        """wendet den Metropolis Algorithmus auf das Gitter an"""
+        """uses the metropolis algorithm on the lattice (1 time step)"""
         x_rand = random.randint(0, self.xdim - 1)
         y_rand = random.randint(0, self.ydim - 1)
 
-        current = self.lattice[x_rand][y_rand]
+        current = self.lattice[x_rand][y_rand] # random lattice site
         new = -current
         up = self.lattice[x_rand][(y_rand + 1) % self.xdim]
         down = self.lattice[x_rand][y_rand - 1]
@@ -47,16 +48,17 @@ class grid:
         energy_old = (-current) * (up + down + left + right + self.h)
         energy_new = (-new) * (up + down + left + right + self.h)
 
-        energy_delta = energy_new - energy_old
+        energy_delta = energy_new - energy_old # energy difference to flip spin
         if(energy_delta < 0):
-            self.lattice[x_rand][y_rand] = new
+            self.lattice[x_rand][y_rand] = new # flip if deltaE<0
         else:
             probability = np.exp(-energy_delta / self.tau)
             if(random.random() < probability):
-                self.lattice[x_rand][y_rand] = new
+                self.lattice[x_rand][y_rand] = new # flip with probability exp(deltaE / tau)
         return
 
     def make_plot(self):
+        """creates animation of current state of lattice"""
         self.fig = plt.figure()
 
         ax = self.fig.add_subplot(111)
@@ -70,6 +72,7 @@ class grid:
         sli_speed = Slider(ax_speed, 'SpeedUp', 1, 100, valinit=1, valstep=1)
 
         def update_fig(*args):
+            """updates plot"""
             self.tau = sli_tau.val
             self.h = sli_h.val
             self.speed = sli_speed.val
